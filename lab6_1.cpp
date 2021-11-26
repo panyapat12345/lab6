331ad12fadd1c387ba888aba05bdab60902b277a
Panyapat Wongdee <panyapat12345@gmail.com>
2021-11-26 15:49:19 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,24 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+
+    int num, num_even = 0, num_odd = 0;
+
+    while(num != 0){
+
+        cout << "Enter an integer: ";
+        cin >> num;
+
+        if(num != 0){
+            if(num%2 == 0)
+                num_even++;
+            else
+                num_odd++;
+        }
+    }
+
+    cout << "#Even numbers = " << num_even << endl;
+    cout << "#Odd numbers = " << num_odd;
+    
     return 0;
 }
